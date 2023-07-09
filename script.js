d6afdee047f6dfa30181e0769d709b758ddbe529
NguyenTu <atunguye25@gmail.com>
2023-07-09 21:05:37 +0700

    First commit

---
+++ script.js
@@ -0,0 +1,104 @@
+const expression = document.getElementById("expression");
+const result = document.getElementById("result");
+
+const numBtns = document.getElementsByClassName("num");
+const operatorBtns = document.getElementsByClassName("operator");
+const operators = new Set(["+", "-", "*", "/"]);
+
+function calculate() {
+  if (
+    operators.has(
+      expression.innerText.charAt(expression.innerText.length - 1) // If the last character is an operator
+    )
+  ) {
+    result.innerText = ""; // Then don't calculate
+  } else {
+    const expressionValue = expression.innerText;
+    let value = eval(expressionValue);
+    value = Math.round((value + Number.EPSILON) * 10000000) / 10000000;
+    result.innerText = "=" + value;
+  }
+}
+
+function updateResultStyle() {
+  result.style.fontSize = "100%";
+  result.style.opacity = "100%";
+}
+
+function resetResultStyle() {
+  result.style.fontSize = "75%";
+  result.style.opacity = "50%";
+}
+
+for (let i = 0; i < numBtns.length; i++) {
+  numBtns[i].addEventListener("click", () => {
+    if (result.style.fontSize === "100%") {
+      // If the result is calculated
+      resetResultStyle();
+      expression.innerText = "";
+    }
+    expression.innerText += numBtns[i].innerText;
+    calculate();
+  });
+}
+
+for (let i = 0; i < operatorBtns.length; i++) {
+  operatorBtns[i].addEventListener("click", () => {
+    // Check to see if it's valid to use an operator
+    // The only valid scenario is when the last character is not an operator and it's not an empty text
+    if (
+      !operators.has(
+        expression.innerText.charAt(expression.innerText.length - 1)
+      ) &&
+      expression.innerText !== ""
+    ) {
+      if (operatorBtns[i].innerText === "=") {
+        updateResultStyle();
+        calculate();
+      } else {
+        expression.innerText += operatorBtns[i].innerText;
+      }
+    }
+  });
+}
+
+// Reset button
+const resetBtn = document.getElementById("reset");
+resetBtn.addEventListener("click", () => {
+  expression.innerText = "";
+  result.innerText = "";
+});
+// Delete button
+const deleteBtn = document.getElementById("delete");
+deleteBtn.addEventListener("click", () => {
+  expression.innerText = expression.innerText.slice(0, -1);
+  if (expression.innerText !== "") {
+    calculate();
+  } else {
+    result.innerText = "";
+  }
+});
+
+// Light-dark mode button
+const swapBtn = document.getElementById("swap");
+function getCurrentTheme() {
+  let theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  }
+  return theme;
+}
+
+swapBtn.addEventListener("click", () => {
+  let theme = getCurrentTheme();
+  if (theme === "dark") {
+    theme = "light";
+  } else {
+    theme = "dark";
+  }
+  localStorage.setItem("theme", theme);
+  const root = document.querySelector(":root");
+  root.setAttribute("color-scheme", theme);
+});
